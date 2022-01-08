afdb00dafc451a9bdb7d14831c03bc2246e1a8df
citixenken <kmuyesu@gmail.com>
2022-01-08 12:17:23 +0300

    first commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ken Muyesu on 08/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
